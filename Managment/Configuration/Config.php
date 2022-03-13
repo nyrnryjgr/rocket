7a9946280b0f494b142eefa61727c805a1449b29
nyrnryjgr <101185623+nyrnryjgr@users.noreply.github.com>
2022-03-13 16:06:12 +0300

    Add files via upload

---
+++ Managment/Configuration/Config.php
@@ -0,0 +1,21 @@
+<?php
+date_default_timezone_set('europe/madrid');
+	define('DB_HOST', 'IP');
+	define('DB_NAME', 'DBName');
+	define('DB_USERNAME', 'DBUser');
+	define('DB_PASSWORD', 'DBPASSWORD');
+	define('ERROR_MESSAGE', '<style>
+body 
+{
+	background-color: black;
+	color:white;
+}
+.alert-danger
+{
+	color:red;
+}
+.alert-success
+{
+	color:lime;
+}
+</style><span style="color: red;">Error db Please Wait!');
